56c007a0ababd06889d66a72f2a4f55ed7919777
Diskmaster (github) / BenRierimanu (nexus) <146549+Diskmaster@users.noreply.github.com>
2021-12-30 23:29:14 -0600

    Script revision 2, thanks mscheeeetham

--- scripts/mscheeeetham-fixPositionAndRotationWildEdits.pas
+++ scripts/mscheeeetham-fixPositionAndRotationWildEdits.pas
@@ -2,70 +2,98 @@
   Supports Fallout 4 only.
 }
 unit fixRoundingErrors;
-  
+uses 'lib\mteFunctions';
+
 	function Initialize: integer;
 	begin
 	end;
 
 	function Process(rec: IInterface): Integer;
-	var
-	  r,refs,ref, baseRef,masRec: IInterface;
-	  overrideRecord, overrideRecordFile: IInterface;
-	  m: IInterface;
-	  curRecFile: IInterface;
-	  el,cont: IInterface;
-	  j,i,k,id,z: integer;
-	  zValrec,zValmas,zValdel: extended;
-	  overrideRecordName,curRecName,baseRefSig: string;	  
 	begin
-		if (Signature(rec) <> 'REFR') then Exit;		
-		curRecName := getfilename(rec);				
-		findElements(rec);
+		if (Signature(rec) <> 'REFR') then Exit;				
+		findElements(rec,rec);
 		// Result := 1;
 	end;
 
 
-	function findElements(rec:IInterface):boolean;
-	var el,recElm,recElms:IInterface;
+	function findElements(el,rec:IInterface):boolean;
+	var currElm,currElms:IInterface;
 	els,xyz:tStringList;
 	  i,j: integer;
-	  path: string;
+	  path,poop: string;
 	begin
-		for i := 0 to Pred(ElementCount(rec)) do begin
-			recElm := ElementByIndex(rec, i);
-			path := name(recElm) + '\';
-			//addmessage('	path: ' + path);
-			if(elementExists(rec,path + 'Position/Rotation')) then path := path + 'Position/Rotation\';
-			if elementExists(rec,path + 'Position')
-			or elementExists(rec,path + 'Rotation') then begin
-				els := TStringList.Create;
-				els.CommaText := 'Position,Rotation';
-				xyz := TStringList.Create;
-				xyz.CommaText := 'Z,X,Y';
-				for i:=0 to 1 do begin					
-					for j:= 0 to 2 do begin
-						checkFloatError(rec, path + els[i] + '\' + xyz[j]);
-					end;
-				end;
+		for i := 0 to Pred(ElementCount(el)) do begin
+			currElm := ElementByIndex(el, i);
+			if (DefType(currElm) = dtStruct) then findElements(currElm,rec);			
+			//addmessage('el: ' + name(el) + elementPath(el));	
+			if (defType(currElm) = dtFloat) then begin
+				//addmessage('Cell: ' + shortname(rec) + ': ' + name(currElm));	
+				checkFloatError(rec, elementPath(currElm));		
 			end;
 		end;				
 	end;	
-
+		
 	function checkFloatError(rec, path:string):boolean;
 	var
 	  el,masRec: IInterface;
 	  elVal,maVal,del: extended;
-	Begin
+	Begin		
 		//addmessage('		path: ' + path);	
 		el := elementByPath(rec,path);
-		elVal := strToFloat(GetElementEditValues(rec,path));
-		maVal := strToFloat(GetElementEditValues(masterOrSelf(rec),path));
+		elVal := GetNativeValue(el);
+		maVal := GetNativeValue(elementByPath(masterOrSelf(rec),path));
 		del := elVal-maVal;
 		//addmessage('	testing ' + path + ' from: ' + FloatToStr(elVal) + ' To: ' + FloatToStr(maVal) + ' with delta: ' + FloatToStr(del));
 		if not (del = 0) then if (del > -0.0002) and (del < 0.0002) then begin
-			addmessage('Cell: ' + name(rec));	
-			addmessage('	Changing ' + path(el) + ' from: ' + FloatToStr(elVal) + ' To: ' + FloatToStr(maVal));
+			addmessage(path(el));
 			setNativeValue(el, maVal);
 		end;
 	end;
 end.
+
+
+{	  {if DefType(recElm) = dtRecord then
+					poop := 'dtRecord'
+				  else if DefType(recElm) = dtSubRecord then
+					poop := 'dtSubRecord'
+				  else if DefType(recElm) = dtSubRecordArray then
+					poop := 'dtSubRecordArray'
+				  else if DefType(recElm) = dtSubRecordStruct then
+					poop := 'dtSubRecordStruct'
+				  else if DefType(recElm) = dtSubRecordUnion then
+					poop := 'dtSubRecordUnion'
+				  else if DefType(recElm) = dtString then
+					poop := 'dtString'
+				  else if DefType(recElm) = dtLString then
+					poop := 'dtLString'
+				  else if DefType(recElm) = dtLenString then
+					poop := 'dtLenString'
+				  else if DefType(recElm) = dtByteArray then
+					poop := 'dtByteArray'
+				  else if DefType(recElm) = dtInteger then
+					poop := 'dtInteger'
+				  else if DefType(recElm) = dtIntegerFormater then
+					poop := 'dtIntegerFormatter'
+				  else if DefType(recElm) = dtFloat then
+					poop := 'dtFloat'
+				  else if DefType(recElm) = dtArray then
+					poop := 'dtArray'
+				  else if DefType(recElm) = dtStruct then
+					poop := 'dtStruct'
+				  else if DefType(recElm) = dtUnion then
+					poop := 'dtUnion'
+				  else if DefType(recElm) = dtEmpty then
+					poop := 'dtEmpty';
+				addmessage(signature(recElm) + ': ' + poop);
+				if DefType(recElm) = dtStruct then findElements(recElm);
+				els := TStringList.Create;
+				els.CommaText := 'Position,Rotation,Bounds';
+				for i:=0 to 1 do begin
+					if elementExists(rec,path + els[i]) then begin
+						el := elementByPath(rec,path + els[i]);
+						for j:= 0 to 2 do begin
+							checkFloatError(rec, path + els[i] + '\' + el);
+						end;
+				
+					end;
+				end}
